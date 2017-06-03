21fc9f5c0a6ca831cab18937b05cd7a02285ee78
Jimitiki <cwjohnson42@gmail.com>
2017-06-03 01:59:37 -0600

    separated graph from a* and added rrt

---
+++ graph.py
@@ -0,0 +1,54 @@
+from mathutils import distance
+from environment import environment
+
+class graph:
+
+    def __init__(self):
+        self.nodes = {}
+        self.path = []
+
+    def add_node(self, node):
+        self.nodes[(node.get_pos_x(), node.get_pos_y())] = node
+
+    def get_node(self, pos_x, pos_y):
+        try:
+            return self.nodes[(pos_x, pos_y)]
+        except:
+            return None
+
+    def get_closest(self, pos_x, pos_y):
+        min_node = None
+        min_distance = float('inf')
+        for (pos, node) in self.nodes:
+            d = distance((pos_x, pos_y), pos)
+            if d < min_distance:
+                min_distance = d
+                min_node = node
+        return min_node
+
+    def build_graph_from_environment(self, env, width, height):
+        return
+
+    def print_graph(self):
+        for pos, node in self.nodes.items():
+            print(str(pos))
+            for adj in node.adjacent:
+                print('  ' + str((adj.pos_x, adj.pos_y)))
+
+class graph_node:
+    def __init__(self, pos_x, pos_y):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.adjacent = []
+
+    def get_pos_x(self):
+        return self.pos_x
+
+    def get_pos_y(self):
+        return self.pos_y
+
+    def get_position(self):
+        return (self.pos_x, self.pos_y)
+
+    def add_adjacent(self, adj_node):
+        self.adjacent.append(adj_node)
---
+++ rrt.py
@@ -0,0 +1,55 @@
+import random
+import mathutils
+from graph import graph, graph_node
+
+class rrt(graph):
+
+    def __init__(self, edge_length):
+        graph.__init__(self)
+        self.max_edge_length = edge_length
+
+    def build_graph_from_environment(self, env, width, height):
+        root = graph_node(env.get_start()[0], env.get_start()[1])
+        self.add_node(root)
+        goal = env.get_goal()
+        end_node = None
+        is_complete = False
+        while not is_complete:
+            point = self.generate_point(env)
+            neighbor = self.get_closest(point[0], point[1])
+            new_node = self.get_new_node(neighbor, point, env)
+            if self.is_valid_edge(new_node, neighbor):
+                new_node.add_adjacent(neighbor)
+                self.add_node(new_node)
+                if mathutils.distance(new_node.get_position, goal):
+                    is_complete = True
+                    end_node = graph_node(goal[0], goal[1])
+                    end_node.add_adjacent(new_node)
+        self.build_path(end_node)
+        return self.path
+
+    def generate_point(self, env):
+        return (0, 0)
+
+    def get_new_node(self, init_node, point, env):
+        node_pos = init_node.get_position()
+        if mathutils.distance(node_pos, point) > self.max_edge_length:
+            #normalize vector difference
+            diff_vector = mathutils.normalize(node_pos[0] - point[0], node_pos[1] - point[1])
+            #multiply normalized vector by the max distance
+            diff_vector[0] *= self.max_edge_length
+            diff_vector[1] *= self.max_edge_length
+            #add to original node
+            new_position = (node_pos[0] + diff_vector[0], node_pos[1] + diff_vector[1])
+            return graph_node(new_position[0], new_position[1])
+
+        return graph_node(point[0], point[1])
+
+    def is_valid_edge(self, node1, node2):
+        return True
+
+    def build_path(self, end_node):
+        cur_node = end_node
+        while len(cur_node.adjacent) > 0:
+            self.path.insert(0, cur_node)
+            cur_node = cur_node.adjacent[0]
